44773b9545a93cbbe73eb5c089897ec8b946b565
John Michael Miguel <poodzia@gmail.com>
2020-10-13 19:16:54 +0800

    Move server.js to src folder

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
-const Person = require("./src/models/person");
+const Person = require("./models/person");
 
 app.use(express.json());
 app.use(express.static("build"));
