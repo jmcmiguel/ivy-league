1d4f0ba2562d31d954160acc0c139d434175c1c8
John Michael Miguel <poodzia@gmail.com>
2020-10-15 23:03:51 +0800

    Refactor ForgotPassword.js

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,54 +1,17 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Paper from "@material-ui/core/Paper";
-import Stepper from "@material-ui/core/Stepper";
-import Step from "@material-ui/core/Step";
-import StepLabel from "@material-ui/core/StepLabel";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
 import ForgotPasswordForm from "../components/ForgotPasswordForm";
 import EmailVerificationForm from "../components/EmailVerificationForm";
 import Copyright from "../components/Copyright";
-
-const useStyles = makeStyles(theme => ({
-  appBar: {
-    position: "relative",
-  },
-  layout: {
-    width: "auto",
-    marginLeft: theme.spacing(2),
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
-      width: 600,
-      marginLeft: "auto",
-      marginRight: "auto",
-    },
-  },
-  paper: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-    padding: theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-      marginTop: theme.spacing(6),
-      marginBottom: theme.spacing(6),
-      padding: theme.spacing(3),
-    },
-  },
-  stepper: {
-    padding: theme.spacing(3, 0, 5),
-  },
-  buttons: {
-    display: "flex",
-    justifyContent: "flex-end",
-  },
-  button: {
-    marginTop: theme.spacing(3),
-    marginLeft: theme.spacing(1),
-  },
-}));
+import useStylesForgotPassword from "../components/styles/useStylesForgotPassword";
+import {
+  CssBaseline,
+  Paper,
+  Stepper,
+  Step,
+  StepLabel,
+  Button,
+  Typography,
+} from "@material-ui/core";
 
 const steps = ["Enter account details", "Wait for email verification"];
 
@@ -64,7 +27,7 @@ function getStepContent(step) {
 }
 
 const ForgotPassword = () => {
-  const classes = useStyles();
+  const classes = useStylesForgotPassword();
   const [activeStep, setActiveStep] = React.useState(0);
 
   const handleNext = () => {
@@ -74,13 +37,6 @@ const ForgotPassword = () => {
   return (
     <React.Fragment>
       <CssBaseline />
-      <AppBar position="absolute" color="default" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            BPSU Online Examination System
-          </Typography>
-        </Toolbar>
-      </AppBar>
       <main className={classes.layout}>
         <Paper className={classes.paper}>
           <Typography component="h1" variant="h4" align="center">
