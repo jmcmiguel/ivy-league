912dc7000755a348bb68a09ce00fa0fd3c3539de
John Michael Miguel <poodzia@gmail.com>
2020-12-25 08:09:45 +0800

    Tweaked index.html

--- frontend/src/components/DashboardTeacher.jsx
+++ frontend/src/components/DashboardTeacher.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import classServices from "../services/classes";
+import examServices from "../services/exams";
 import DashboardCharts from "../components/DashboardCharts";
 import useStylesTeacherHome from "../components/styles/useStylesTeacherHome";
 import clsx from "clsx";
@@ -6,84 +8,91 @@ import clsx from "clsx";
 import { Box, Typography, Paper, Container, Divider } from "@material-ui/core";
 
 const DashboardTeacher = () => {
-  // const [sampleData, setSampleData] = useState();
-  // const [exams, setExams] = useState();
-  // const [classess, setClassess] = useState();
+  const [sampleData, setSampleData] = useState();
+  const [exams, setExams] = useState();
+  const [classess, setClassess] = useState();
 
-  // useState(() => {
-  //   // Get prof classes
-  //   classServices
-  //     .getProfClass(localStorage.getItem("email"))
-  //     .then(classes => {
-  //       setClassess(classes);
+  useState(() => {
+    // Get prof classes
+    classServices
+      .getProfClass(localStorage.getItem("email"))
+      .then(classes => {
+        setClassess(classes);
 
-  //       // Get Prof exams
-  //       examServices
-  //         .getProfExams(localStorage.getItem("email"))
-  //         .then(exams => {
-  //           setExams(exams);
+        // Get Prof exams
+        examServices
+          .getProfExams(localStorage.getItem("email"))
+          .then(exams => {
+            setExams(exams);
+            let dummyData = {};
 
-  //           let dummyData = {};
-  //         })
-  //         .catch(err => console.log(err.message));
-  //     })
+            classess.map(classes => {
+              return {
+                section: classes.section,
+                subject: classes.courseCode,
+                desc: classes.courseCode,
+              };
+            });
+          })
+          .catch(err => console.log(err.message));
+      })
 
-  //     .catch(err => console.log(err.message));
-  // }, []);
+      .catch(err => console.log(err.message));
+  }, []);
 
-  const sampleData = [
-    {
-      section: "NW3D",
-      subject: "ICTC-1213",
-      desc: "Software Engineering 1",
-      highestScorer: "Sanchez, Rick",
-      topScore: 100,
-      chartData: [
-        { examname: "Exam 1", avgscore: 20 },
-        { examname: "Exam 2", avgscore: 10 },
-        { examname: "Exam 3", avgscore: 30 },
-        { examname: "Exam 4", avgscore: 50 },
-        { examname: "Exam 5", avgscore: 80 },
-      ],
-      tableData: [
-        {
-          rank: 1,
-          idNumber: "18-0xxxx",
-          name: "Rick Sanchez",
-          avgScore: 102,
-          ttlScore: 9000,
-        },
-        {
-          rank: 2,
-          idNumber: "20-0xxxx",
-          name: "Morty Smith",
-          avgScore: 10,
-          ttlScore: 90,
-        },
-        {
-          rank: 3,
-          idNumber: "14-0xxxx",
-          name: "Light Yagami",
-          avgScore: 400,
-          ttlScore: 10000,
-        },
-        {
-          rank: 4,
-          idNumber: "16-0xxxx",
-          name: "Tanjiro Kamado",
-          avgScore: 1123,
-          ttlScore: 901230,
-        },
-        {
-          rank: 5,
-          idNumber: "18-0xxxx",
-          name: "Nezuko-Chan",
-          avgScore: 1023,
-          ttlScore: 9420,
-        },
-      ],
-    },
-  ];
+  // const sampleData = [
+  //   {
+  //     section: "NW3D",
+  //     subject: "ICTC-1213",
+  //     desc: "Software Engineering 1",
+  //     highestScorer: "Sanchez, Rick",
+  //     topScore: 100,
+  //     chartData: [
+  //       { examname: "Exam 1", avgscore: 20 },
+  //       { examname: "Exam 2", avgscore: 10 },
+  //       { examname: "Exam 3", avgscore: 30 },
+  //       { examname: "Exam 4", avgscore: 50 },
+  //       { examname: "Exam 5", avgscore: 80 },
+  //     ],
+  //     tableData: [
+  //       {
+  //         rank: 1,
+  //         idNumber: "18-0xxxx",
+  //         name: "Rick Sanchez",
+  //         avgScore: 102,
+  //         ttlScore: 9000,
+  //       },
+  //       {
+  //         rank: 2,
+  //         idNumber: "20-0xxxx",
+  //         name: "Morty Smith",
+  //         avgScore: 10,
+  //         ttlScore: 90,
+  //       },
+  //       {
+  //         rank: 3,
+  //         idNumber: "14-0xxxx",
+  //         name: "Light Yagami",
+  //         avgScore: 400,
+  //         ttlScore: 10000,
+  //       },
+  //       {
+  //         rank: 4,
+  //         idNumber: "16-0xxxx",
+  //         name: "Tanjiro Kamado",
+  //         avgScore: 1123,
+  //         ttlScore: 901230,
+  //       },
+  //       {
+  //         rank: 5,
+  //         idNumber: "18-0xxxx",
+  //         name: "Nezuko-Chan",
+  //         avgScore: 1023,
+  //         ttlScore: 9420,
+  //       },
+  //     ],
+  //   },
+  // ];
 
   const classes = useStylesTeacherHome();
   const fixedHeightPaper = clsx(classes.paper);
