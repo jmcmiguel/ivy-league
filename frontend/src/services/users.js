d4701f7c996d22f8dcc4eb4e8543e005dac58d10
John Michael Miguel <poodzia@gmail.com>
2021-01-12 19:14:33 +0800

    Fixed baseURL

--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:8080";
+const baseURL = "";
 
 const signup = async newUser => {
   const request = axios.post(`${baseURL}/api/signup`, newUser);
